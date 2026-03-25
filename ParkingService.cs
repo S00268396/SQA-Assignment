64b70575871d6d38705bc1d6b0287bd8a42421af
Emilia Dobrucka <S00268396@atu.ie>
2026-03-25 10:32:04 +0000

    Updata 25/03/26

--- ParkingService.cs
+++ ParkingService.cs
@@ -22,7 +22,7 @@ namespace SQA_Week10
             double fee;
 
             if (vehicleType == "standard")
-                if ((hours >= 1) && (hours < 3))
+                if ((hours >= 1) && (hours <= 3))
                     fee = hours * 4.0;
                 else if (hours >= 4)
                     fee = hours * 3.0;
@@ -51,10 +51,9 @@ namespace SQA_Week10
 
     public class Tests
     {
-
-
+        //Standard_ReturnsTests
         [Test]
-        public void GetParkingFee_Returns()
+        public void TenHourStandarad_Returns()
         {
             // Arrange
             var mockDiscountService = new Mock<IDiscountService>();
@@ -67,6 +66,130 @@ namespace SQA_Week10
             // Assert
             //Assert.AreEqual("It's hot!", weatherDescription); // classic
             Assert.That(fee, Is.EqualTo(27)); // constraint
+        }                   
+
+        [Test]
+        public void ThreeHourStandard_Returns()
+        {
+            // Arrange
+            var mockDiscountService = new Mock<IDiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(.1);
+            var parkingService = new ParkingService(mockDiscountService.Object);
+
+            // Act
+            double fee = parkingService.CalculateFee(3, "standard");
+
+            // Assert
+            //Assert.AreEqual("It's hot!", weatherDescription); // classic
+            Assert.That(fee, Is.EqualTo(12)); // constraint
+        }
+
+        [Test]
+        public void SevenHourStandard_Returns()
+        {
+            // Arrange
+            var mockDiscountService = new Mock<IDiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(.1);
+            var parkingService = new ParkingService(mockDiscountService.Object);
+
+            // Act
+            double fee = parkingService.CalculateFee(7, "standard");
+
+            // Assert
+            //Assert.AreEqual("It's hot!", weatherDescription); // classic
+            Assert.That(fee, Is.EqualTo(21)); // constraint
+        }
+
+        //Electric - Returns
+        [Test]
+        public void ThreeHourElectric_Returns()
+        {
+            // Arrange
+            var mockDiscountService = new Mock<IDiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(.1);
+            var parkingService = new ParkingService(mockDiscountService.Object);
+
+            // Act
+            double fee = parkingService.CalculateFee(3, "electric");
+
+            // Assert
+            Assert.That(fee, Is.EqualTo(9)); // constraint
+        }
+
+        [Test]
+        public void SevenHourElectric_Returns()
+        {
+            // Arrange
+            var mockDiscountService = new Mock<IDiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(.1);
+            var parkingService = new ParkingService(mockDiscountService.Object);
+
+            // Act
+            double fee = parkingService.CalculateFee(7, "electric");
+
+            // Assert
+            Assert.That(fee, Is.EqualTo(14)); // constraint
+        }
+
+        [Test]
+        public void TenHourElectric_Returns()
+        {
+            // Arrange
+            var mockDiscountService = new Mock<IDiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(.1);
+            var parkingService = new ParkingService(mockDiscountService.Object);
+
+            // Act
+            double fee = parkingService.CalculateFee(10, "electric");
+
+            // Assert
+            Assert.That(fee, Is.EqualTo(18)); // constraint
+        }
+
+        //Invalid Vehicle
+        [Test]
+        public void ThreeHourInvalidVehicle_Returns()
+        {
+            // Arrange
+            var mockDiscountService = new Mock<IDiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(.1);
+            var parkingService = new ParkingService(mockDiscountService.Object);
+
+            // Act
+            double fee = parkingService.CalculateFee(3, "Truck");
+
+            // Assert
+            Assert.That(fee, Is.EqualTo(0)); // constraint
+        }
+        [Test]
+        public void InvalidHourElectric_Returns()
+        {
+            // Arrange
+            var mockDiscountService = new Mock<IDiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(.1);
+            var parkingService = new ParkingService(mockDiscountService.Object);
+
+            // Act
+            double fee = parkingService.CalculateFee(-1, "electric");
+
+            // Assert
+            Assert.That(fee, Is.EqualTo(0)); // constraint
+        }
+
+        [Test]
+        public void InvalidHourStandard_Returns()
+        {
+            // Arrange
+            var mockDiscountService = new Mock<IDiscountService>();
+            mockDiscountService.Setup(x => x.GetDiscount()).Returns(.1);
+            var parkingService = new ParkingService(mockDiscountService.Object);
+
+            // Act
+            double fee = parkingService.CalculateFee(-1, "standard");
+
+            // Assert
+            //Assert.AreEqual("It's hot!", weatherDescription); // classic
+            Assert.That(fee, Is.EqualTo(0)); // constraint
         }
     }
 }
